f96d014b8b6662e48b43cc0ccd2e03b97d1dce6e
ViniciusRibeiro <viniribeiroa@gmail.com>
2023-08-21 00:11:57 -0300

    create class DBhelper

---
+++ app/src/main/java/com/stormdev/listadetarefas/helper/DbHelper.java
@@ -0,0 +1,42 @@
+package com.stormdev.listadetarefas.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static int VERSION = 1;
+    public static String NOME_DB = "DB_TAREFAS";
+    public static String TABELA_TAREFAS = "tarefas";
+    public DbHelper(Context context) {
+        super(context, NOME_DB, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABELA_TAREFAS +
+                " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " nome NOT NULL ); ";
+
+        try{
+
+            db.execSQL(sql
+            );
+            Log.i("INFO DB", "sucesso ao criar a tabela" );
+
+        }catch (Exception e){
+            Log.i("INFO DB", "Error ao criar a tabela" + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+}
