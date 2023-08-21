6b616d2642bcba387dc976bc233f30264561fee1
ViniciusRibeiro <viniribeiroa@gmail.com>
2023-08-20 21:53:11 -0300

    create menu adicionar tarefas

--- app/src/main/java/com/stormdev/listadetarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/stormdev/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,11 @@
 package com.stormdev.listadetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.stormdev.listadetarefas.R;
 
@@ -12,5 +15,21 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.itemSalvar){
+            //Execultar um codigo
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
